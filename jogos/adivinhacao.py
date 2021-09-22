13b779621a70f360e424bfbe09b9c67345fc63e0
Cezar Rosa <cecez86@gmail.com>
2021-09-22 09:32:15 -0300

    - wip: aula 2 e jogo de adivinhação

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,1 +1,19 @@
+print("*********************************")
 print("Bem-vindo ao jogo de adivinhação!")
+print("*********************************")
+
+numero_secreto = 22
+
+# entrada sempre vem como str
+chute_str = input("Digite seu número:")
+
+# conversão str para int
+chute = int(chute_str)
+
+print("Você digitou", chute)
+
+# para comparação ter sentido, variáveis precisam ter o mesmo tipo
+if(numero_secreto == chute):
+    print("Parabéns, certa resposta!")
+else:
+    print("ERROU! Mais sorte para você.")
